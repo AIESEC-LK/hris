c1c312274693494427b0feab5fb26a54adbebebc
kaneeldias <kaneeldias@hotmail.com>
2021-09-07 02:26:34 +0530

    Add angular fire and auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {environment} from "../environments/environment";
 
 import { AppComponent } from './app.component';
 import { RouterModule } from "@angular/router";
@@ -8,6 +9,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { LoginComponent } from './auth/login/login.component';
+
+// Angular fire
+import {AngularFireModule} from "@angular/fire/compat";
+import {SETTINGS as AUTH_SETTINGS, AngularFireAuthModule} from "@angular/fire/compat/auth";
+
 
 
 @NgModule({
@@ -20,9 +26,13 @@ import { LoginComponent } from './auth/login/login.component';
         RouterModule,
         AppRoutingModule,
         FlexLayoutModule,
-        BrowserAnimationsModule
+        BrowserAnimationsModule,
+        AngularFireModule.initializeApp(environment.firebase, "angular-auth-firebase"),
+        AngularFireAuthModule
     ],
-  providers: [],
+  providers: [
+    { provide: AUTH_SETTINGS, useValue: { appVerificationDisabledForTesting: true } },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
