04fe58d5a2362991a73f0ad78ae0d713ffefb0aa
kaneeldias <kaneeldias@hotmail.com>
2022-12-15 19:33:53 +0530

    Add opportunity analytics support

--- src/app/opportunity/view-opportunity/view-opportunity.component.ts
+++ src/app/opportunity/view-opportunity/view-opportunity.component.ts
@@ -1,78 +1,96 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute, Router, NavigationEnd} from "@angular/router";
-import {AuthService} from "../../auth/auth.service";
-import {MatDialog} from "@angular/material/dialog";
-import {ErrorComponent} from "../../dialogs/error/error.component";
-import {Opportunity, OpportunityService} from "../opportunity.service";
-import {LoadingComponent} from "../../dialogs/loading/loading.component";
-import { AngularFireAnalytics } from '@angular/fire/compat/analytics';
-import {Title} from "@angular/platform-browser";
+import { ActivatedRoute, Router, NavigationEnd } from "@angular/router";
+import { AuthService } from "../../auth/auth.service";
+import { MatDialog } from "@angular/material/dialog";
+import { ErrorComponent } from "../../dialogs/error/error.component";
+import { Opportunity, OpportunityService } from "../opportunity.service";
+import { LoadingComponent } from "../../dialogs/loading/loading.component";
+import { Title } from "@angular/platform-browser";
+import { AnalyticsService, EventSummary } from 'src/app/analytics.service';
 
 
 @Component({
-  selector: 'app-view-opportunity',
-  templateUrl: './view-opportunity.component.html',
-  styleUrls: ['./view-opportunity.component.css']
+	selector: 'app-view-opportunity',
+	templateUrl: './view-opportunity.component.html',
+	styleUrls: ['./view-opportunity.component.css']
 })
 export class ViewOpportunityComponent implements OnInit {
 
-  opportunity?: Opportunity;
-  loading = true;
-  isLoggedIn:boolean = false;
+	opportunity?: Opportunity;
+	loading = true;
+	isLoggedIn: boolean = false;
 
-  private sub: any;
+	analyticsSummary?: EventSummary[];
 
-  constructor(private route: ActivatedRoute, public authService:AuthService,
-              public opportunityService: OpportunityService, private dialog: MatDialog, private router:Router, private analytics: AngularFireAnalytics,
-              private titleService:Title) {
-  }
+	private sub: any;
 
-  async ngOnInit(): Promise<void> {
-    // Temporarily disable login check to allow non-added users
-    // if (!await this.authService.isLoggedIn()) await this.authService.login();    
-    this.isLoggedIn = await this.authService.isLoggedIn();
+	constructor(private route: ActivatedRoute, public authService: AuthService,
+		public opportunityService: OpportunityService, private dialog: MatDialog, private router: Router, private analytics: AnalyticsService,
+		private titleService: Title) {
+	}
 
-    this.router.events.subscribe((evt) => {
-      if (!(evt instanceof NavigationEnd)) {
-          return;
-      }
-      window.scrollTo(0, 0)
-    });
+	async ngOnInit(): Promise<void> {
+		// Temporarily disable login check to allow non-added users
+		// if (!await this.authService.isLoggedIn()) await this.authService.login();    
+		this.isLoggedIn = await this.authService.isLoggedIn();
 
-    try { 
-      this.sub = this.route.params.subscribe(async params => {
-        let id = params['id'];
-        this.opportunity = undefined;
-        this.opportunity = await this.opportunityService.getOpportunity(id);
-        this.titleService.setTitle(`${this.opportunity!.title} | ASL 360°`);
-        this.analytics.logEvent('opportunity.view', {"id": this.opportunity!.id});
-       });
-    } catch (e) {
-      this.dialog.open(ErrorComponent, {data: e});
-    }
+		this.router.events.subscribe((evt) => {
+			if (!(evt instanceof NavigationEnd)) {
+				return;
+			}
+			window.scrollTo(0, 0)
+		});
 
-    this.loading = false;
-  }
+		try {
+			this.sub = this.route.params.subscribe(async params => {
+				let id = params['id'];
+				this.opportunity = undefined;
+				this.analyticsSummary = undefined;
+				this.opportunity = await this.opportunityService.getOpportunity(id);
+				this.titleService.setTitle(`${this.opportunity!.title} | ASL 360°`);
+				this.analytics.logEvent('opportunity.view', {id: this.opportunity!.id});
+		
+				console.log(this.authService.isEBOrAbove());
+		
+				if (this.authService.isEBOrAbove()) {
+					this.analyticsSummary = await this.analytics.getEventSummary({id: this.opportunity!.id});
+				}
+		
+			});
+		} catch (e) {
+			this.dialog.open(ErrorComponent, { data: e });
+		}
 
-  async delete(opportunity: Opportunity) {
-    let loadingDialog = this.dialog.open(LoadingComponent);
-    try {
-      await this.opportunityService.deleteOpportunity(opportunity);
-      await this.router.navigate(["/opportunities"]);
-    } catch (e) {
-      this.dialog.open(ErrorComponent, {data: e});
-    } finally {
-      loadingDialog.close();
-    }
-    return;
-  }
+		this.loading = false;
+	}
 
-  async logApplyClick() {
-    this.analytics.logEvent('opportunity.click', {"id": this.opportunity?.id});
-  }
+	async delete(opportunity: Opportunity) {
+		let loadingDialog = this.dialog.open(LoadingComponent);
+		try {
+			await this.opportunityService.deleteOpportunity(opportunity);
+			await this.router.navigate(["/opportunities"]);
+		} catch (e) {
+			this.dialog.open(ErrorComponent, { data: e });
+		} finally {
+			loadingDialog.close();
+		}
+		return;
+	}
 
-  ngOnDestroy() {
-    this.sub.unsubscribe();
-  }
+	async logApplyClick() {
+		this.analytics.logEvent('opportunity.click', { "id": this.opportunity?.id });
+	}
+
+	getViews() {
+		return this.analyticsSummary!.find(o => o.event_name === "opportunity.view")!.count;
+	}
+
+	getClicks() {
+		return this.analyticsSummary!.find(o => o.event_name === "opportunity.click")!.count;
+	}
+
+	ngOnDestroy() {
+		this.sub.unsubscribe();
+	}
 
 }
